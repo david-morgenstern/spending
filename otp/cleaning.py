27fc139c7813cd936a1e1ee7a25584fb7e236e8e
daveyo89 <j.tothdavid@gmail.com>
2022-02-01 18:09:32 +0100

    basics

---
+++ otp/cleaning.py
@@ -0,0 +1,25 @@
+from typing import List
+
+import pandas as pd
+
+
+class Cleaner:
+
+    def __init__(self, file_paths: List[str]):
+        self.file_paths = file_paths
+
+    def csv_to_dataframe(self) -> pd.DataFrame:
+        df_list = [
+            pd.read_csv(file_path,
+                        delimiter=";", skipinitialspace=True,
+                        names=
+                        [1, 2, 'amount', 'currency', 5, 'date', 7, 8, 'target', 'comment', 11, 12, 'type', 14, 15],
+                        index_col=None, header=0) for file_path in self.file_paths]
+
+        df = pd.concat(df_list, axis=0, ignore_index=True)
+        df.drop(axis=1, columns=[1, 2, 5, 7, 8, 11, 12, 14, 15], inplace=True)
+        df['date'] = df['date'].fillna(method='ffill')
+        df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+        df['target'].fillna(df['type'], inplace=True)
+
+        return df
