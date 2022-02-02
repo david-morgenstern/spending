064bedcfc34b026c26e89b314ce119affcf55233
daveyo89 <j.tothdavid@gmail.com>
2022-02-02 22:04:38 +0100

    iso format date in json

--- otp/dataframe_operations.py
+++ otp/dataframe_operations.py
@@ -1,9 +1,14 @@
 from pandas import DataFrame
 
 
-def get_by_date_group(df: DataFrame) -> DataFrame:
-    return df.groupby('date', as_index=True)['amount'].sum().to_frame()
+class Operations:
+    def __init__(self, df: DataFrame):
+        self.df = df
 
+    @property
+    def get_by_date_group(self) -> DataFrame:
+        return self.df.groupby('date', as_index=True)['amount'].sum().to_frame()
 
-def get_by_target_group(df: DataFrame) -> DataFrame:
-    return df.groupby('target', as_index=True)['amount'].sum().to_frame()
+    @property
+    def get_by_target_group(self) -> DataFrame:
+        return self.df.groupby('target', as_index=True)['amount'].sum().to_frame()
--- otp/views.py
+++ otp/views.py
@@ -4,12 +4,18 @@ import os
 from django.http import HttpResponse
 
 from .dataframe_cleaner import csv_to_dataframe
-from .dataframe_operations import get_by_date_group, get_by_target_group
+from .dataframe_operations import Operations
+
 
 def home(request):
     csv_files = ['data/1.csv']
     result = csv_to_dataframe(csv_files)
-    parsed = json.loads(result.to_json())
-    result_json = {'table': parsed, 'date_group': json.loads(get_by_date_group(result).to_json()), 'target_group': json.loads(get_by_target_group(result).to_json())}
-    return HttpResponse(json.dumps(result_json, indent=4))
+    parsed = json.loads(result.to_json(date_format='iso'))
 
+    ops = Operations(result)
+    by_date = ops.get_by_date_group
+    by_target = ops.get_by_target_group
+
+    result_json = {'table': parsed, 'date_group': json.loads(by_date.to_json(date_format='iso')),
+                   'target_group': json.loads(by_target.to_json(date_format='iso'))}
+    return HttpResponse(json.dumps(result_json, indent=4))
